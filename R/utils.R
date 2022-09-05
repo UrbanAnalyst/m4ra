072e283cd1a17f602a7c9981cd99913d841bef57
mpadge <mark.padgham@email.com>
2022-09-05 11:49:40 +0200

    add weight-networks.R

--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,30 @@ m4ra_cache_dir <- function () {
             "R",
             "m4ra"
         ))
+        
+        if (!dir.exists (cache_dir)) {
+            dir.create (cache_dir, recursive = TRUE)
+        }
     }
 
     return (cache_dir)
+}
+
+#' Return a unique hash for the input network.
+#'
+#' This can be used to identify cached versions of a given network, in
+#' particular to determine whether or not weighted versions of a given network
+#' have been previously cached.
+#'
+#' @inheritParams m4ra
+#' @return Single character value with unique hash of given network.
+#' @export
+m4ra_network_hash <- function (net) {
+
+    checkmate::assert_class (net, "osmdata_sc")
+
+    full_hash <- digest::digest (net)
+    hash <- substr (full_hash, 1L, 6L)
+
+    return (hash)
 }
---
+++ R/weight-networks.R
@@ -0,0 +1,37 @@
+#' Cache weighted networks for routing query
+#'
+#' Uses a default cache location specified by `rappdirs::user_cache_dir()`. This
+#' location can be over-ridden by specifying a local environment variable,
+#' "M4RA_CACHE_DIR".
+#'
+#' @inheritParams m4ra
+#' @param quiet If `FALSE`, display progress information on screen.
+#' @return A character vector of local locations of cached versions of the
+#' variously weighted network representations used in \link{m4ra}.
+#' @export
+m4ra_weight_networks <- function (net, quiet = TRUE) {
+
+    wt_profiles <- c ("foot", "bicycle")
+    hash <- m4ra_network_hash (net)
+
+    filenames <- NULL
+
+    for (w in wt_profiles) {
+
+        filename <- file.path (
+            m4ra_cache_dir (),
+            paste0 ("mr4a-", hash, "-", w, ".Rds")
+        )
+
+        if (!quiet) {
+            cli::cli_alert_info ("Weighting network with '{w}' profile")
+        }
+        net_w <- dodgr::weight_streetnet (net, wt_profile = w)
+
+        fst::write_fst (net_w, filename)
+
+        filenames <- c (filenames, filename)
+    }
+
+    return (filenames)
+}
