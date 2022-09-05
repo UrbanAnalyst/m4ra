d46bfaee88b77569885d2a7431dd516f0c2f62fb
mpadge <mark.padgham@email.com>
2022-09-05 16:22:39 +0200

    fix tests

--- tests/testthat/test-weight-networks.R
+++ tests/testthat/test-weight-networks.R
@@ -6,6 +6,21 @@ Sys.setenv ("M4RA_CACHE_DIR" = tempdir ())
 
 test_that ("weight networks", {
 
+    flist <- list.files (
+        tempdir (),
+        pattern = "^m4ra\\-",
+        full.names = TRUE
+    )
+    chks <- "not null"
+    if (length (flist) > 0L) {
+        chks <- tryCatch (
+            file.remove (flist),
+            error = function (e) NULL
+        )
+    }
+
+    skip_if (is.null (chks)) # shouldn't happen
+
     expect_message (
         f <- m4ra_weight_networks (m4ra_hampi, quiet = FALSE),
         "Weighting network with"
