ed97a999a90df036b7b6a1c18e28bf32a9483f55
mpadge <mark.padgham@email.com>
2022-09-05 12:10:06 +0200

    add internal hampi sc data

--- R/m4ra-package.R
+++ R/m4ra-package.R
@@ -2,6 +2,25 @@
 #' @aliases m4ra-package
 "_PACKAGE"
 
+#' m4ra_hampi
+#'
+#' A sample street network from the township of Hampi, Karnataka, India. The
+#' same as the `dodgr::hampi` dataset, but in `sc` rather than `sf` format.
+#'
+#' @name m4ra_hampi
+#' @docType data
+#' @family data
+#' @keywords datasets
+#' @format A \pkg{silicate} `sc` representation of the street
+#' network of Hampi.
+#'
+#' @note Can be re-created with the following commands.
+#' @examples
+#' \dontrun{
+#' hampi <- dodgr_streetnet_sc ("hampi india")
+#' }
+NULL
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
