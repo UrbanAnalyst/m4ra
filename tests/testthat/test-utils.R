c00d37dbbbfd71b67d84958a4e145323118d7403
mpadge <mark.padgham@email.com>
2022-09-05 12:40:15 +0200

    test non-tempdir cache dir on gha

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -12,3 +12,21 @@ test_that ("cache dir", {
     expect_true (dir.exists (d))
     expect_equal (d, tempdir ())
 })
+
+
+skip_if (!test_all)
+
+test_that ("cache dir", {
+
+    d0 <- m4ra_cache_dir ()
+    Sys.unsetenv ("M4RA_CACHE_DIR")
+    d1 <- m4ra_cache_dir ()
+
+    expect_length (d0, 1L)
+    expect_length (d1, 1L)
+    expect_true (d0 != d1)
+    expect_true (d0 == tempdir ())
+    expect_true (d1 != tempdir ())
+
+    unlink (d1, recursive = TRUE)
+})
