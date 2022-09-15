eef65c7eebc343124e486acbdec938073acab3d8
mpadge <mark.padgham@email.com>
2022-09-15 21:03:25 +0200

    add src/pathfinders for #1

---
+++ src/pathfinders.cpp
@@ -0,0 +1,160 @@
+#include "pathfinders.h"
+#include "heap.h"
+
+#include <algorithm> // std::fill
+
+// Modified from code by Shane Saunders
+
+// @param n Number of vertices in graph
+// @param heapD The type of heap used
+// @param g A DGraph object
+// @param twoheap If `TRUE`, uses a bi-directional search.
+PF::PathFinder::PathFinder(size_t n,
+        const HeapDesc& heapD,
+        std::shared_ptr<const DGraph> g)
+{
+    m_heap = heapD.newInstance (n);
+    m_closed = new bool [n];
+    m_open = new bool [n];
+    init (g);
+}
+
+PF::PathFinder::~PathFinder() {
+    delete [] m_open;
+    delete [] m_closed;
+    delete m_heap;
+}
+
+void PF::PathFinder::init(std::shared_ptr<const DGraph> g) {
+    m_graph = g;
+}
+
+void PF::PathFinder::init_arrays (
+        std::vector <double>& d,
+        std::vector <double>& w,
+        std::vector <long int>& prev,
+        bool *m_open_vec,
+        bool *m_closed_vec,
+        const size_t v,
+        const size_t n)
+{
+    std::fill (w.begin (), w.end (), INFINITE_DOUBLE);
+    std::fill (d.begin (), d.end (), INFINITE_DOUBLE);
+    std::fill (prev.begin (), prev.end (), INFINITE_INT);
+    w [v] = 0.0;
+    d [v] = 0.0;
+    prev [v] = -1;
+
+    std::fill (m_open_vec, m_open_vec + n, false);
+    std::fill (m_closed_vec, m_closed_vec + n, false);
+    m_open_vec [v] = true;
+}
+
+void PF::PathFinder::scan_edges (const DGraphEdge *edge,
+        std::vector<double>& d,
+        std::vector<double>& w,
+        std::vector<long int>& prev,
+        bool *m_open_vec,
+        const bool *m_closed_vec,
+        const size_t &v0)
+{
+    while (edge) {
+        size_t et = edge->target;
+        if (!m_closed_vec [et])
+        {
+            double wt = w [v0] + edge->wt;
+            if (wt < w [et]) {
+                d [et] = d [v0] + edge->dist;
+                w [et] = wt;
+                prev [et] = static_cast <int> (v0);
+
+                if (m_open_vec [et]) {
+                    m_heap->decreaseKey(et, wt);
+                }
+                else {
+                    m_heap->insert (et, wt);
+                    m_open_vec [et] = true;
+                }
+            } else
+                m_closed [et] = true;
+        }
+        edge = edge->nextOut;
+    }
+}
+
+void PF::PathFinder::scan_edges_heur (const DGraphEdge *edge,
+        std::vector<double>& d,
+        std::vector<double>& w,
+        std::vector<long int>& prev,
+        bool *m_open_vec,
+        const bool *m_closed_vec,
+        const size_t &v0,
+        const std::vector<double> &heur)    // heuristic for A*
+{
+    while (edge) {
+        size_t et = edge->target;
+        if (!m_closed_vec [et])
+        {
+            double wt = w [v0] + edge->wt;
+            if (wt < w [et]) {
+                d [et] = d [v0] + edge->dist;
+                w [et] = wt;
+                prev [et] = static_cast <int> (v0);
+
+                if (m_open_vec [et]) {
+                    m_heap->decreaseKey(et, wt + heur [et] - heur [v0]);
+                }
+                else {
+                    m_heap->insert (et, wt + heur [et] - heur [v0]);
+                    m_open_vec [et] = true;
+                }
+            } else
+                m_closed [et] = true;
+        }
+        edge = edge->nextOut;
+    }
+}
+
+/**********************************************************************
+ ************************  PATH ALGORITHMS    *************************
+ **********************************************************************/
+
+
+void PF::PathFinder::AStar (std::vector<double>& d,
+        std::vector<double>& w,
+        std::vector<long int>& prev,
+        const std::vector<double>& heur,
+        const size_t v0,
+        const std::vector <size_t> &to_index)
+{
+    const DGraphEdge *edge;
+
+    const size_t n = m_graph->nVertices();
+    const std::vector<DGraphVertex>& vertices = m_graph->vertices();
+
+    PF::PathFinder::init_arrays (d, w, prev, m_open, m_closed, v0, n);
+    m_heap->insert (v0, heur [v0]);
+
+    size_t n_reached = 0;
+    const size_t n_targets = to_index.size ();
+    bool *is_target = new bool [n];
+    std::fill (is_target, is_target + n, false);
+    for (auto t: to_index)
+        is_target [t] = true;
+
+    while (m_heap->nItems() > 0) {
+        size_t v = m_heap->deleteMin();
+
+        m_closed [v] = true;
+        m_open [v] = false;
+
+        edge = vertices [v].outHead;
+        scan_edges_heur (edge, d, w, prev, m_open, m_closed, v, heur);
+
+        if (is_target [v])
+            n_reached++;
+        if (n_reached == n_targets)
+            break;
+    } // end while m_heap->nItems
+    delete [] is_target;
+}
---
+++ src/pathfinders.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <set>
+#include <cmath> // fabs
+#include <unordered_set> // used in bidirected search
+
+// inf_dbl used only in AStar2:
+#include <limits>
+
+#include "dgraph.h"
+
+constexpr long int INFINITE_INT =  std::numeric_limits <long int>::max ();
+constexpr double INFINITE_DOUBLE =  std::numeric_limits <double>::max ();
+
+class Heap;
+class HeapDesc;
+class DGraph;
+
+namespace PF{
+
+/* DijkstraEdge is used for the std::set implementation, everything else is for
+ * Shane Saunders's heap sort versions */
+struct DijkstraEdge
+{
+    DijkstraEdge (double wt, size_t i): _wt (wt), _i (i) {}
+    
+    double _wt;
+    size_t _i;
+
+    size_t geti () const { return _i;   }
+    double getw () const { return _wt;   }
+};
+
+struct by_wt
+{
+    bool operator () (const DijkstraEdge& lhs, const DijkstraEdge& rhs)
+    {
+        if (fabs (lhs._wt - rhs._wt) < 1.0e-12)
+            return lhs._i < rhs._i;
+        else
+            return lhs._wt < rhs._wt;
+    }
+}; 
+
+typedef std::set <DijkstraEdge, by_wt> EdgeSet;
+
+
+class PathFinder {
+    public:
+        PathFinder (size_t n,
+                const HeapDesc& heapD,
+                std::shared_ptr<const DGraph> g);
+        ~PathFinder();
+        
+        // Disable copy/assign as will crash
+        PathFinder(const PathFinder&) = delete;
+        PathFinder& operator=(const PathFinder&) = delete;
+
+        void init (std::shared_ptr<const DGraph> g);
+        void init_arrays (
+                std::vector<double>& d,
+                std::vector<double>& w,
+                std::vector<long int>& prev,
+                bool *m_open_vec,
+                bool *m_closed_vec,
+                const size_t v,
+                const size_t n);
+        void scan_edges (
+                const DGraphEdge *edge,
+                std::vector<double>& d,
+                std::vector<double>& w,
+                std::vector<long int>& prev,
+                bool *m_open_vec,
+                const bool *m_closed_vec,
+                const size_t &v0);
+        void scan_edges_heur ( // with A* heuristic
+                const DGraphEdge *edge,
+                std::vector<double>& d,
+                std::vector<double>& w,
+                std::vector<long int>& prev,
+                bool *m_open_vec,
+                const bool *m_closed_vec,
+                const size_t &v0,
+                const std::vector<double> &heur);
+        // with A* heuristic for dists-categorical
+        void scan_edge_types_heur (
+                const DGraphEdge *edge,
+                std::vector<double>& d,
+                std::vector<double>& w,
+                std::vector<long int>& prev,
+                bool *m_open_vec,
+                const bool *m_closed_vec,
+                const size_t &v0,
+                const std::vector<double> &heur);
+        // run_sp_categorical for threshold dists
+        void scan_edge_types (
+                const DGraphEdge *edge,
+                std::vector<double>& d,
+                std::vector<double>& w,
+                std::vector<long int>& prev,
+                bool *m_open_vec,
+                const bool *m_closed_vec,
+                const size_t &v0);
+
+        void AStar (std::vector<double>& d,
+                std::vector<double>& w,
+                std::vector<long int>& prev,
+                const std::vector<double>& heur,
+                const size_t v0,
+                const std::vector <size_t> &to_index);
+
+    private:
+        Heap *m_heap;        // pointer: heap
+        // Convert to vector<bool>? (save memory, might be a performace hit though)
+        bool *m_open;           // array: frontier set state of vertices
+        bool *m_closed;         // also for bi-dir
+
+        std::shared_ptr<const DGraph> m_graph;    // pointer: directed graph    
+
+        EdgeSet edge_set;
+};
+
+} // end namespace PF
