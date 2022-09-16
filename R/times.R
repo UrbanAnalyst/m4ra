b679afa51feaad96f6e0e0effa4f1503cc927935
mpadge <mark.padgham@email.com>
2022-09-16 13:11:21 +0200

    split off new R/dodgr-internal-fns.R file

--- R/times.R
+++ R/times.R
@@ -27,17 +27,6 @@ m4ra_times <- function (graph,
         stop ("'graph' must be a 'dodgr_streetnet_sc' object")
     }
 
-    # ------- `dodgr` internal functions:
-    tbl_to_df <- utils::getFromNamespace ("tbl_to_df", "dodgr")
-    graph_cols <- utils::getFromNamespace ("dodgr_graph_cols", "dodgr")
-    get_turn_penalty <- utils::getFromNamespace ("get_turn_penalty", "dodgr")
-    create_compound_junctions <-
-        utils::getFromNamespace ("create_compound_junctions", "dodgr")
-    make_vert_map <- utils::getFromNamespace ("make_vert_map", "dodgr")
-    get_to_from_index <- utils::getFromNamespace ("get_to_from_index", "dodgr")
-    to_from_with_tp <- utils::getFromNamespace ("to_from_with_tp", "dodgr")
-    # ------- end `dodgr` internal functions:
-
     graph <- tbl_to_df (graph)
 
     gr_cols <- graph_cols (graph)
