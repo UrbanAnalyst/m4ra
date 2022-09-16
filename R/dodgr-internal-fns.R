104bef600a5b5ea301933de2ed5dcfb307a07855
mpadge <mark.padgham@email.com>
2022-09-16 13:56:38 +0200

    internally define dodgr:::get_turn_penalty - it's not in current CRAN version

--- R/dodgr-internal-fns.R
+++ R/dodgr-internal-fns.R
@@ -4,8 +4,6 @@ tbl_to_df <- utils::getFromNamespace ("tbl_to_df", "dodgr")
 
 graph_cols <- utils::getFromNamespace ("dodgr_graph_cols", "dodgr")
 
-get_turn_penalty <- utils::getFromNamespace ("get_turn_penalty", "dodgr")
-
 create_compound_junctions <-
     utils::getFromNamespace ("create_compound_junctions", "dodgr")
 
@@ -14,3 +12,13 @@ make_vert_map <- utils::getFromNamespace ("make_vert_map", "dodgr")
 get_to_from_index <- utils::getFromNamespace ("get_to_from_index", "dodgr")
 
 to_from_with_tp <- utils::getFromNamespace ("to_from_with_tp", "dodgr")
+
+# This is only in versions > v0.2.15
+# get_turn_penalty <- utils::getFromNamespace ("get_turn_penalty", "dodgr")
+get_turn_penalty <- function (graph) {
+    tp <- attr (graph, "turn_penalty")
+    if (!is.numeric (tp)) {
+        tp <- 0.0
+    }
+    return (tp)
+}
