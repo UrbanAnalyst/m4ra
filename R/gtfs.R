bc4ef27032fb94763b15df24fac861c2193cff76
mpadge <mark.padgham@email.com>
2022-09-23 18:27:57 +0200

    add gtfs_traveltimes fn for #5

---
+++ R/gtfs.R
@@ -0,0 +1,50 @@
+
+#' Construct a travel time matrix between all pairs of stops in a 'GTFS' feed.
+#'
+#' @param gtfs A 'GTFS' feed extracted with the \pkg{gtfsrouter} function,
+#' 'extract_gtfs'.
+#' @param start_time_limits As for the 'gtfs_traveltimes' function of
+#' \pkg{gtfsrouter}, a vector of two values specifying the earliest and latest
+#' departure times from each station.
+#' @param day As for the 'gtfs_traveltimes' function of \pkg{gtfsrouter}, the
+#' day for which the matrix is to be calculated.
+#' @return An integer matrix of fastest travel times between all stops for the
+#' specified 'start_time_limits'
+#' @export
+
+m4ra_gtfs_traveltimes <- function (gtfs, start_time_limits, day) {
+
+    if (!"timetable" %in% names (gtfs)) {
+        gtfs <- gtfsrouter::gtfs_timetable (gtfs)
+    }
+
+    stops <- unique (gtfs$stops$stop_id)
+
+    num_cores <- parallel::detectCores ()
+
+    tt <- parallel::mclapply (stops, function (s) {
+
+        out <- rep (NA_integer_, length (stops))
+
+        tt <- gtfs_traveltimes (
+            gtfs,
+            from = s,
+            start_time_limits = start_time_limits,
+            from_is_id = TRUE
+        )
+        if (nrow (tt) == 0L) {
+            return (out)
+        }
+
+        index <- match (tt$stop_id, stops)
+        out [index] <- tt$duration
+
+        return (out)
+    }, mc.cores = num_cores)
+
+    tt <- do.call (rbind, tt)
+
+    rownames (tt) <- colnames (tt) <- stops
+
+    return (tt)
+}
