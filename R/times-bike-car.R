e819516ca23fc32703caed3ba3b85c8c07bdc550
mpadge <mark.padgham@email.com>
2022-10-21 15:12:14 +0200

    rewrite times-bike-car for vectors of 'from' points for #7

--- R/times-bike-car.R
+++ R/times-bike-car.R
@@ -47,37 +47,50 @@ m4ra_bike_car_times <- function (city = NULL, from = NULL) {
     graph_c <- graph_c [graph_c$component == 1, ]
     v_c <- dodgr::dodgr_vertices (graph_c)
 
+    if (is.null (from)) {
+        # Get vertices common to all networks:
+        vert_count <- table (c (
+            unique (graph_f$.vx0),
+            unique (graph_b$.vx0),
+            unique (graph_c$.vx0)
+        ))
+        verts_all <- names (vert_count) [which (vert_count == 3)]
+        v <- v_f [which (v_f$id %in% verts_all), ]
+
+        from <- v$id
+        message ("Calculating times from [", length (from), "] vertices")
+    }
+
     v <- rbind (v_f, v_b, v_c)
     ids <- unique (v$id)
     v <- v [match (ids, v$id), ]
 
     car_times <- m4ra_times_single_mode (graph_c, from = from)
-    car_times <- data.frame (
-        id = colnames (car_times),
-        car_t = car_times [1, ]
-    )
+    car_times <- data.frame (t (car_times))
+    car_times <- cbind (id = rownames (car_times), car_times)
 
     bike_times <- m4ra_times_single_mode (graph_b, from = from)
-    bike_times <- data.frame (
-        id = colnames (bike_times),
-        bike_t = bike_times [1, ]
-    )
+    bike_times <- data.frame (t (bike_times))
+    bike_times <- cbind (id = rownames (bike_times), bike_times)
 
     walk_dists <- dodgr::dodgr_distances (graph_f, from = from)
-    walk_dists <- data.frame (
-        id = colnames (walk_dists),
-        walk_d = walk_dists [1, ]
-    )
+    walk_dists <- data.frame (t (walk_dists))
+    walk_dists <- cbind (id = rownames (walk_dists), walk_dists)
 
-    dat <- dplyr::left_join (car_times, bike_times, by = "id") |>
-        dplyr::left_join (walk_dists, by = "id") |>
-        dplyr::mutate (ratio = bike_t / car_t) |>
-        dplyr::mutate (walk_d = walk_d / 1000) |>
-        dplyr::filter (!is.na (ratio) & !is.na (walk_d)) |>
-        dplyr::left_join (v, by = "id") |>
-        dplyr::select (-c ("component", "n"))
+    ids <- table (c (car_times$id, bike_times$id, walk_dists$id))
+    ids <- names (ids) [which (ids == 3)]
+    car_times <- car_times [match (ids, car_times$id), -1]
+    bike_times <- bike_times [match (ids, bike_times$id), -1]
+    walk_dists <- walk_dists [match (ids, walk_dists$id), -1] / 1000
 
-    return (dat)
+    ratio <- bike_times / car_times
+    v <- v [match (ids, v$id), ]
+
+    return (list (
+        dist = walk_dists,
+        ratio = ratio,
+        verts = v
+    ))
 }
 
 #' Convert bicycle and automobile times into equivalent areas for a specified
@@ -88,16 +101,16 @@ m4ra_bike_car_times <- function (city = NULL, from = NULL) {
 #' ratio of bicycle to automobile times is less than specified limit.
 #' @family analyses
 #' @export
-m4ra_bike_car_ratio_areas <- function (bike_car_dat, ratio_lims = 1:20 / 5) {
+m4ra_bike_car_ratio_areas <- function (bike_car_dat, ratio_lims = 1 + 0:10 / 10) {
 
     requireNamespace ("geosphere")
 
-    ratio_lims <- ratio_lims [which (ratio_lims < max (bike_car_dat$ratio))]
+    ratio_lims <- ratio_lims [which (ratio_lims < max (bike_car_dat$ratio, na.rm = TRUE))]
 
     # Convex hull area (in square km) for one ratio:
-    ratio_area <- function (dat, ratio_lim = 1.0) {
+    ratio_area <- function (ratios, verts, ratio_lim = 1.0) {
 
-        xy <- as.matrix (dat [which (dat$ratio <= ratio_lim), c ("x", "y")])
+        xy <- as.matrix (verts [which (ratios <= ratio_lim), c ("x", "y")])
         xy <- xy [chull (xy), , drop = FALSE]
         area <- 0
         if (nrow (xy) > 3L) {
@@ -106,14 +119,24 @@ m4ra_bike_car_ratio_areas <- function (bike_car_dat, ratio_lims = 1:20 / 5) {
         return (area)
     }
 
-    ratio_areas <- vapply (ratio_lims, function (r)
-                           ratio_area (bike_car_dat, ratio_lim = r),
-                           numeric (1L))
+    nverts <- ncol (bike_car_dat$ratio)
+    ratio_areas <- lapply (seq_len (nverts), function (i) {
+        vapply (ratio_lims, function (r)
+            ratio_area (bike_car_dat$ratio [, i], bike_car_dat$verts, r),
+            numeric (1L))
+    })
+    ratio_areas <- do.call (rbind, ratio_areas)
 
-    res <- data.frame (
-        ratio = ratio_lims,
-        area = ratio_areas
-    )
+    # coordinates:
+    from_names <- colnames (bike_car_dat$ratio)
+    index <- match (gsub ("^X", "", from_names), bike_car_dat$verts$id)
+    xy <- bike_car_dat$verts [index, ]
+    # then append ratios as columns onto that:
+    n0 <- ncol (xy)
+    dat <- cbind (xy, ratio_areas)
+    cnames <- paste0 ("r", sprintf ("%1.1f", ratio_lims))
+    index <- seq_len (ncol (dat)) [-seq_len (n0)]
+    names (dat) [index] <- cnames
 
-    return (res)
+    return (dat)
 }
