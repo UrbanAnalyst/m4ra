3f2f7c232d27005de8f43d688446efab6d28e4ad
mpadge <mark.padgham@email.com>
2022-11-07 13:02:58 +0100

    finish first cut of multi-modal algorithm for #8

--- R/times-multi-modal.R
+++ R/times-multi-modal.R
@@ -2,86 +2,64 @@
 #' Calculate matrix of pair-wise travel times between points using multiple
 #' modes of transport.
 #'
-#' @param graph A `dodgr` network returned from the \link{weight_streetnet}
-#' function using a network obtained with the \pkg{osmdata} `osmdata_sc`
-#' function.
-#' @param gtfs A 'GTFS' feed extracted with the \pkg{gtfsrouter} function,
-#' 'extract_gtfs'.
-#' @param from Vector or matrix of points **from** which route distances are to
-#' be calculated. If not given, times are calculated from all points in the
-#' network.
-#' @param start_time_limits As for the 'gtfs_traveltimes' function of
-#' \pkg{gtfsrouter}, a vector of two values specifying the earliest and latest
-#' departure times from each station.
-#'
+#' @param initial_mode Initial mode of transport from origin points towards
+#' public transport stop.
+#' @param from List of OSM vertex IDs from which to calculate total multi-modal
+#' travel times. These must be vertices from the largest connected component of
+#' the contracted graph.
+#' @inheritParams m4ra_prepare_data
 #' @family main
 #' @export
-m4ra_times_multi_mode <- function (graph,
-                                   gtfs,
+m4ra_times_multi_mode <- function (net_sc = NULL, gtfs = NULL, city_name = NULL,
+                                   day = NULL, start_time_limits = NULL,
+                                   initial_mode = "foot", final_mode = "foot",
                                    from = NULL,
-                                   start_time_limits = NULL) {
+                                   fast = FALSE, n_closest = 10L, quiet = FALSE) {
 
-    # Final times from GTFS stops out to network points are only calcualted from
-    # this many potential stops for each start point. See the C++ code in
-    # "travel_times" for details. Varying this value should not make any great
-    # difference, except for slowing down algorithm for larger values.
-    n_closest <- 10L
-
-    message (cli::symbol$play,
-        cli::col_green (" Calculating times to all GTFS stops "),
-        appendLF = FALSE)
-    utils::flush.console ()
-    tmat_net_gtfs <- m4ra_times_to_gtfs_stops (
-        graph,
-        gtfs,
-        from = from,
-        graph_to_gtfs = TRUE
+    files <- m4ra_prepare_data (
+        net_sc = net_sc,
+        gtfs = gtfs,
+        city_name = city_name,
+        day = day,
+        start_time_limits = start_time_limits,
+        final_mode = final_mode,
+        fast = fast,
+        n_closest = n_closest,
+        quiet = quiet
     )
-    message ("\r", cli::col_green (cli::symbol$tick,
-        " Calculated times to all GTFS stops   "))
 
-    message (cli::symbol$play,
-        cli::col_green (" Calculating times between all GTFS stops "),
-        appendLF = FALSE)
-    utils::flush.console ()
-    tmat_gtfs_gtfs <-
-        m4ra_gtfs_traveltimes (gtfs, start_time_limits = start_time_limits)
-    message ("\r", cli::col_green (cli::symbol$tick,
-        " Calculated times between all GTFS stops   "))
+    graph <- m4ra_load_cached_network (city = city_name, mode = initial_mode)
+    stops <- readRDS (gtfs)$stops
+    f <- grep ("gtfs\\-.*[0-9]{5}\\-[0-9]{5}\\.Rds$", files, value = TRUE)
+    gtfs_mat <- readRDS (f)
+    f <- grep ("gtfs\\-to\\-net", files, value = TRUE)
+    gtfs_to_net <- readRDS (f)
 
-    message (cli::symbol$play,
-        cli::col_green (" Calculating times back out to all network points "),
-        appendLF = FALSE)
-    utils::flush.console ()
-    tmat_gtfs_net <-
-        m4ra_times_to_gtfs_stops (graph, gtfs, graph_to_gtfs = FALSE)
-    message ("\r", cli::col_green (cli::symbol$tick,
-        " Calculated times back out to all network points   "))
-
-    # The C++ function then traces for each "from" point the shortest times
-    # to all gtfs stations, then selects the `n_closest` of them, and traces
-    # times from those terminal stops out to all remaining network points,
-    # updating any shortest times found.
-    v <- dodgr::dodgr_vertices (graph)
-    if (!is.null (from)) {
-        v <- v [match (from, v$id), ]
+    graph_c <- dodgr::dodgr_contract_graph (graph)
+    graph_c <- graph_c [graph_c$component == 1L, ]
+    v <- dodgr::dodgr_vertices (graph_c)
+    if (!all (from %in% v$id)) {
+        stop (
+            "Not all specified 'from' vertices are part of the ",
+            "largest connected component of the street network."
+        )
     }
-    closest_stns <- rcpp_closest_gtfs (v, gtfs$stops, n_closest = n_closest)
 
-    message (cli::symbol$play,
-        cli::col_green (" Combining matrices to generate final travel times "),
-        appendLF = FALSE)
-    utils::flush.console ()
-    final_times <- rcpp_net_gtfs_travel_times (
-        tmat_net_gtfs,
-        tmat_gtfs_gtfs,
-        tmat_gtfs_net,
-        closest_stns
-    )
-    message ("\r", cli::col_green (cli::symbol$tick,
-        " Combined matrices to generate final travel times   "))
+    to <- v$id [dodgr::match_points_to_verts (
+        v, stops [, c ("stop_lon", "stop_lat")])]
+    times <- m4ra_times_single_mode (graph_c, from = from, to = to)
+    n_closest <- update_n_closest (v, stops, n_closest = 10L)
 
-    return (final_times)
+    # Then convert initial times to nearest GTFS stops to times through entire
+    # GTFS network to all termimal network vertices:
+    res <- rcpp_add_net_to_gtfs (times, gtfs_mat, gtfs_to_net$index, gtfs_to_net$d, nrow (v))
+    maxr <- rcpp_matrix_max (res)
+    res [res == maxr] <- NA
+
+    rownames (res) <- from
+    colnames (res) <- v$id
+
+    return (res)
 }
 
 #' Calculate matrix of pair-wise travel times between points using multiple
@@ -91,10 +69,23 @@ m4ra_times_multi_mode <- function (graph,
 #' @inherit m4ra_times_multi_mode
 #' @family main
 #' @export
-m4ra_times_multi_modal <- function (graph,
-                                   gtfs,
+m4ra_times_multi_modal <- function (net_sc = NULL, gtfs = NULL, city_name = NULL,
+                                   day = NULL, start_time_limits = NULL,
+                                   initial_mode = "foot", final_mode = "foot",
                                    from = NULL,
-                                   start_time_limits = NULL) {
+                                   fast = FALSE, n_closest = 10L, quiet = FALSE) {
 
-    m4ra_times_multi_mode (graph, gtfs, from = from, start_time_limits = start_time_limits)
+    m4ra_times_multi_mode (
+        net_sc = net_sc,
+        gtfs = gtfs,
+        city_name = city_name,
+        day = day,
+        start_time_limits = start_time_limits,
+        initial_mode = initial_mode,
+        final_mode = final_mode,
+        from = from,
+        fast = fast,
+        n_closest = n_closest,
+        quiet = quiet
+    )
 }
