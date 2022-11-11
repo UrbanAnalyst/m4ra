281aaa9c809bbad2de647b559b655759b9b67d76
mpadge <mark.padgham@email.com>
2022-11-11 12:05:05 +0100

    fix -Wconversion warnings in multi-modal.cpp

--- src/multi-modal.cpp
+++ src/multi-modal.cpp
@@ -11,8 +11,8 @@ Rcpp::NumericMatrix rcpp_add_net_to_gtfs (Rcpp::NumericMatrix net_times,
         Rcpp::NumericMatrix gtfs_times, Rcpp::List gtfs_to_net_index,
         Rcpp::List gtfs_to_net_dist, const int nverts)
 {
-    const int nfrom = net_times.nrow ();
-    const int n_gtfs = net_times.ncol ();
+    const size_t nfrom = static_cast <size_t> (net_times.nrow ());
+    const size_t n_gtfs = static_cast <size_t> (net_times.ncol ());
 
     if (gtfs_times.nrow () != gtfs_times.ncol () &&
             gtfs_times.nrow () != n_gtfs)
@@ -20,16 +20,16 @@ Rcpp::NumericMatrix rcpp_add_net_to_gtfs (Rcpp::NumericMatrix net_times,
         Rcpp::stop ("GTFS and travel time matrices are not compatible");
     }
 
-    Rcpp::NumericMatrix times_to_end_stops (nfrom, n_gtfs);
+    Rcpp::NumericMatrix times_to_end_stops (static_cast <int> (nfrom), static_cast <int> (n_gtfs));
     std::fill (times_to_end_stops.begin (), times_to_end_stops.end (), INFINITE_DBL);
 
-    for (int i = 0; i < nfrom; i++) {
+    for (size_t i = 0; i < nfrom; i++) {
 
-        for (int j = 0; j < n_gtfs; j++) {
+        for (size_t j = 0; j < n_gtfs; j++) {
 
             const double time_i_to_j = net_times (i, j);
 
-            for (int k = 0; k < n_gtfs; k++) {
+            for (size_t k = 0; k < n_gtfs; k++) {
 
                 const double time_j_to_k = gtfs_times (j, k);
                 const double time_i_to_k = std::min (net_times (i, k), time_i_to_j + time_j_to_k);
@@ -46,23 +46,24 @@ Rcpp::NumericMatrix rcpp_add_net_to_gtfs (Rcpp::NumericMatrix net_times,
     // lists of closest stops to each network point to calculate final fastest
     // times to each terminal network point.
 
-    Rcpp::NumericMatrix res (nfrom, nverts);
+    Rcpp::NumericMatrix res (static_cast <int> (nfrom), nverts);
     std::fill (res.begin (), res.end (), INFINITE_DBL);
 
-    for (int i = 0; i < nfrom; i++)
+    for (size_t i = 0; i < nfrom; i++)
     {
-        for (int j = 0; j < gtfs_to_net_index.size (); j++)
+        for (size_t j = 0; j < gtfs_to_net_index.size (); j++)
         {
             Rcpp::IntegerVector index_j = gtfs_to_net_index (j);
             Rcpp::NumericVector d_j = gtfs_to_net_dist (j);
 
-            for (int k = 0; k < index_j.size (); k++)
+            for (size_t k = 0; k < index_j.size (); k++)
             {
                 const double time_i_to_k = times_to_end_stops (i, j) + d_j (k);
+                const size_t index_k = static_cast <size_t> (index_j [static_cast <R_xlen_t> (k)]);
 
-                if (time_i_to_k < res (i, index_j [k]))
+                if (time_i_to_k < res (i, index_k))
                 {
-                    res (i, index_j [k]) = time_i_to_k;
+                    res (i, index_k) = time_i_to_k;
                 }
             }
         }
