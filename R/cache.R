bcdb0b073390d0f051b6f23faab4d2d7169e53b6
mpadge <mark.padgham@email.com>
2022-11-17 11:47:55 +0100

    add m4ra_cache_network fn for #10

--- R/cache.R
+++ R/cache.R
@@ -13,7 +13,7 @@ m4ra_cache_dir <- function () {
             "R",
             "m4ra"
         ))
-        
+
         if (!dir.exists (cache_dir)) {
             dir.create (cache_dir, recursive = TRUE)
         }
@@ -40,6 +40,57 @@ m4ra_network_hash <- function (net) {
     hash <- substr (full_hash, 1L, 6L)
 
     return (hash)
+}
+
+#' Save a weighted street network in both full and contracted forms to local
+#' cache.
+#'
+#' This uses \pkg{fst}, which strips all attributes, so they are saved
+#' separately, and re-attached in `load_cached_network()`.
+#'
+#' @param net The full network to be cached.
+#' @param city Name of city; used to name cached network files.
+#' @family cache
+#' @export
+m4ra_cache_network <- function (net, city) {
+
+    city <- tolower (city)
+
+    # Full graph:
+    a <- names (attributes (net))
+    a <- a [which (!a %in% c ("names", "row.names", "col.names", "px"))]
+    a <- lapply (a, function (i) attr (net, i))
+
+    hash <- attr (net, "hash")
+    fname <- paste0 ("m4ra-", city, "-attr-", substring (hash, 1, 6), ".Rds")
+    fpath <- file.path (tempdir (), fname)
+
+    saveRDS (a, fpath)
+
+    fst::write_fst (net, gsub ("attr", "net", fpath))
+
+    # Contracted graph:
+    netc <- dodgr::dodgr_contract_graph (net)
+    a <- names (attributes (netc))
+    a <- a [which (!a %in% c ("names", "row.names", "col.names", "px"))]
+    a <- lapply (a, function (i) attr (net, i))
+
+    hashc <- attr (netc, "hashc")
+    fname <- paste0 ("m4ra-", city, "-attr-", substring (hashc, 1, 6), ".Rds")
+    fpath <- file.path (tempdir (), fname)
+
+    saveRDS (a, fpath)
+
+    fst::write_fst (netc, gsub ("attr", "netc", fpath))
+
+    # Edge map and junctions:
+    flist <- list.files (tempdir (), pattern = hashc, full.names = TRUE)
+    edge_map <- readRDS (grep ("edge\\_map", flist, value = TRUE))
+    fst::write_fst (edge_map, gsub ("attr", "edge-map", fpath))
+    junctions <- readRDS (grep ("junctions", flist, value = TRUE))
+    if (length (junctions) > 0L) {
+        fst::write_fst (junctions, gsub ("attr", "junctions", fpath))
+    }
 }
 
 #' Load cached file for one city and mode
