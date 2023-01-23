1b1a5ce86ae7ea87956f41f3196b157c2a50100d
mpadge <mark.padgham@email.com>
2023-01-23 15:00:21 +0100

    convert final Rcpp::List in 'rcpp_add_net_to_gtfs' to vec<vec> for #15

--- src/multi-modal.cpp
+++ src/multi-modal.cpp
@@ -93,30 +93,42 @@ Rcpp::NumericMatrix rcpp_add_net_to_gtfs (Rcpp::NumericMatrix net_times,
     Rcpp::NumericMatrix res (static_cast <int> (nfrom), nverts);
     std::fill (res.begin (), res.end (), INFINITE_DBL);
 
+    // convert the Rcpp::Lists into std::vecs:
+    std::vector <std::vector <size_t> > gtfs_to_net_index_vec (gtfs_to_net_index.size ());
+    std::vector <std::vector <double> > gtfs_to_net_dist_vec (gtfs_to_net_dist.size ());
+
+    for (size_t i = 0; i < gtfs_to_net_index.size (); i++)
+    {
+        Rcpp::IntegerVector index_i = gtfs_to_net_index (i);
+        gtfs_to_net_index_vec [i] = Rcpp::as <std::vector <size_t> > (index_i);
+
+        Rcpp::NumericVector d_i = gtfs_to_net_dist (i);
+        gtfs_to_net_dist_vec [i] = Rcpp::as <std::vector <double> > (d_i);
+    }
+
     for (size_t i = 0; i < nfrom; i++)
     {
 
         Rcpp::checkUserInterrupt ();
 
-        for (size_t j = 0; j < gtfs_to_net_index.size (); j++)
+        for (size_t j = 0; j < gtfs_to_net_index_vec.size (); j++)
         {
             if (times_to_end_stops (i, j) == INFINITE_DBL)
             {
                 continue;
             }
 
-            Rcpp::IntegerVector index_j = gtfs_to_net_index (j);
-            Rcpp::NumericVector d_j = gtfs_to_net_dist (j);
+            const size_t n_j = gtfs_to_net_index_vec [j].size ();
 
-            for (size_t k = 0; k < index_j.size (); k++)
+            for (size_t k = 0; k < n_j; k++)
             {
-                if (d_j (k) < 0)
+                if (gtfs_to_net_dist_vec [j] [k] < 0)
                 {
                     continue;
                 }
 
-                const double time_i_to_k = times_to_end_stops (i, j) + d_j (k);
-                const size_t index_k = static_cast <size_t> (index_j [static_cast <R_xlen_t> (k)]);
+                const double time_i_to_k = times_to_end_stops (i, j) + gtfs_to_net_dist_vec [j] [k];
+                const size_t index_k = gtfs_to_net_index_vec [j] [k];
 
                 if (time_i_to_k < res (i, index_k))
                 {
